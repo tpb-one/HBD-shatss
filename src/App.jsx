81a4f5dcedc22583ffcfb439f9ab068b0b3c3204
theprathambatra <123197020+theprathambatra@users.noreply.github.com>
2026-03-18 00:40:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -170,7 +170,7 @@ function App() {
         </button>
         <Gallery isActive={currentPage === 4} />
         <section className="final">
-          <h2 className="final-message">💖 Forever Yours — [Your Name] 💖</h2>
+          <h2 className="final-message"> Yours Truly — [Pratham Batra] 💖</h2>
           <p className="final-subtitle">Your personalized closing message ✨</p>
         </section>
       </div>
