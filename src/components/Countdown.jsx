44f4f875401de8fda8af34ffbb37be3fe7ba7d6f
tpb-one <theprathambatra@gmail.com>
2026-07-25 12:34:08 +0530

    Update Countdown.jsx

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -92,15 +92,6 @@ function Countdown({ onBirthdayReached, birthdayReached }) {
           label="Minutes"
           prevValue={prevTime.minutes}
         />
-        {/*  TEST BUTTON  */}
-<button
-  className="test-button"
-  onClick={onBirthdayReached}
-  title="Skip countdown and see celebration"
->
-  ��� Test Celebration
-</button>
-{/*  END TEST BUTTON  */}
         <Digit
           value={time.seconds}
           label="Seconds"
