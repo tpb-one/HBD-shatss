e46dd9f758cea32414aba67752a8456d541aa51c
theprathambatra <123197020+theprathambatra@users.noreply.github.com>
2026-07-25 11:49:06 +0530

    Update birthday message in MessageCard component

--- src/components/MessageCard.jsx
+++ src/components/MessageCard.jsx
@@ -13,7 +13,7 @@ function MessageCard({ isActive }) {
   const curtainHintRef = useRef(null);
   const messageContentRef = useRef(null);
 
-  const message = `Shatss,
+  const message = `Babla Rawat,
 
 Happy Birthday Shatss, 
 Wishing you love luck and success.
